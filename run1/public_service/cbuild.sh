90cf5975d744e62543901f2995abb6db638cf087
Tim Wu <changtimwu@gmail.com>
2024-09-29 04:19:54 +0800

    chore: useful command to check the built image

--- run1/public_service/cbuild.sh
+++ run1/public_service/cbuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #gcloud artifacts repositories create samples --repository-format docker
 gcloud builds submit --config cloudbuild.yaml .
+defprj=$(gcloud config get project)
+defloc=$(gcloud config get artifacts/location)
+reponam="samples"
+#gcloud artifacts docker images describe ${defloc}-docker.pkg.dev/${defprj}/${reponam}/public-service-sample1
 
